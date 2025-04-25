84d3ab734fd809fb871eff61ffe1f9757e1c29ca
arillllpaul <rohmanamiril305@gmail.com>
2025-04-25 14:58:20 +0700

    Delete model directory

--- model/kategori.go
+++
@@ -1,7 +0,0 @@
-package model
-
-// Definisi struct Kategori dengan huruf kapital
-type Kategori struct {
-    ID   int    `json:"id"`
-    Nama string `json:"nama"`
-}
--- model/produk.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Produk struct {
-    ID       int     `json:"id"`
-    Nama     string  `json:"nama"`
-    Harga    float64 `json:"harga"`
-    Kategori string  `json:"kategori"`
-}
